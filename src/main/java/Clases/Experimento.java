20e10cbeb5ba14563ed9c054dc8f2824d40494b2
NlodeL <nlodelop@myuax.com>
2024-05-04 15:00:57 +0200

    Creo la nueva clase bacteria para poder tener mas de una poblacion por experimento

--- src/main/java/Clases/Experimento.java
+++ src/main/java/Clases/Experimento.java
@@ -1,34 +1,22 @@
 package Clases;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Experimento {
-    private String nombre;
-    private Date fechaInicio;
-    private Date fechaFin;
-    private int numBacteriasIniciales;
-    private double temperatura;
-    private String condicionesLuminosidad;
-    private int comidaInicial;
-    private int diaIncrementoComida;
-    private int comidaDiaIncremento;
-    private int comidaFinal;
+    private List<Bacteria> bacterias;
 
-    public Experimento(String nombre, Date fechaInicio, Date fechaFin, int numBacteriasIniciales, double temperatura, String condicionesLuminosidad, int comidaInicial, int diaIncrementoComida, int comidaDiaIncremento, int comidaFinal) {
-        this.nombre = nombre;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.numBacteriasIniciales = numBacteriasIniciales;
-        this.temperatura = temperatura;
-        this.condicionesLuminosidad = condicionesLuminosidad;
-        this.comidaInicial = comidaInicial;
-        this.diaIncrementoComida = diaIncrementoComida;
-        this.comidaDiaIncremento = comidaDiaIncremento;
-        this.comidaFinal = comidaFinal;
+    public Experimento() {
+        this.bacterias = new ArrayList<>();
     }
 
-    public int calcularComida(int dia) {
-        // Implementar la lógica para calcular la cantidad de comida en el día dado
-        return 0; // Cambiar esto por la implementación correcta
+    public void agregarBacteria(Bacteria bacteria) {
+        this.bacterias.add(bacteria);
+    }
+
+    public List<Bacteria> obtenerBacterias() {
+        return this.bacterias;
     }
 }
+
