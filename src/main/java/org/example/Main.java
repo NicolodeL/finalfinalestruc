012d2a83493ff5cdee202e197c5753f5425adb88
NlodeL <nlodelop@myuax.com>
2024-05-04 15:12:08 +0200

    empiezo la creacion de interfaz grafica en el main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new MainFrame();
+            frame.setSize(800, 600);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
     }
 }
